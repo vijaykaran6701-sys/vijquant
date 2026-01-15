6adef886010f889c940b1ca2d0bd651863a48df6
vijaykaran6701-sys <vijaykaran6701@gmail.com>
2026-01-15 13:59:14 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // 🔥 IMPORTANT for Cloudflare Pages
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
+
+  plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
